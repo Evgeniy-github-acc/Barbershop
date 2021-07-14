06c158862321db7ef5e5e4debb3e08a5e537b83b
Evgeniy-github-acc <3374555@gmail.com>
2021-07-14 13:17:00 +0300

    some pics added

--- app.rb
+++ app.rb
@@ -77,8 +77,7 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
-	erb 'Can you handle a <a href="/secure/place">secret</a>?'
+	erb ""			
 end
 
 get '/about' do
