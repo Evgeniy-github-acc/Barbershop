40f07df02a5b5a1ee4f06edf879091cd21f1c5e7
Evgeniy-github-acc <3374555@gmail.com>
2021-07-14 12:35:37 +0300

    add validation to contact form

--- app.rb
+++ app.rb
@@ -20,6 +20,9 @@ class Barbers < ActiveRecord::Base
 end
 
 class Contact < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :message, presence: true 
 end
 
 before do
@@ -113,21 +116,30 @@ post '/contacts' do
 	
 	@contact = Contact.new params[:contact]
 	@contact.save
-
-	  Pony.mail(:to => '3374555@mail.ru', :from => "#{@contact.email}", :subject => "Сообщение от #{@contact.name}", :body => "#{@contact.message}",   :via_options => {
-			:address              => 'smtp.gmail.com',
-			:port                 => '587',
-			:enable_starttls_auto => true,
-			:user_name            => 'eo0065110@gmail.com',
-			:password             => 'eo0065110_google',
-			:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-			:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-		  })
-	
-
-	@title = "Успешно!"
-	@message = "Ваше сообщение отправлено и будет обработано в ближайшее время!"
-	erb :message	  
+  
+	if @contact.save
+		Pony.mail(:to => '3374555@mail.ru', 
+				  :from => "#{@contact.email}", 
+				  :subject => "Сообщение от #{@contact.name}", 
+				  :body => "#{@contact.message}",   
+				  :via_options => {
+								:address              => 'smtp.gmail.com',
+								:port                 => '587',
+								:enable_starttls_auto => true,
+								:user_name            => 'eo0065110@gmail.com',
+								:password             => 'eo0065110_google',
+								:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+								:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+	 							}
+				)		
+		@title = "Успешно!"
+			@message = "Ваше сообщение отправлено и будет обработано в ближайшее время!"
+			erb :message
+	else
+		@error = @contact.errors.full_messages.first
+		erb :contacts
+	end
+		  
 
 end 
 
